5842a359be3b11153c8c90aaad9c095421159cd5
Neobis <neobis@iMac-Neobis.local>
2022-06-22 16:25:41 +0600

    added buttons and images, added new view and view controller

--- Gallery App/ViewController.swift
+++ Gallery App/ViewController.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var watermelon: UIButton!
+    @IBOutlet weak var strawberry: UIButton!
+    @IBOutlet weak var orange: UIButton!
+    @IBOutlet weak var bananas: UIButton!
+    
+    @IBAction func imagesTouched(_ sender: UIButton) {
+        switch sender {
+        case bananas:
+            print("bananas")
+        case orange:
+            print("orange")
+        case strawberry:
+            print("strawberry")
+        case watermelon:
+            print("watermelon")
+        default:
+            print("no buttons")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
