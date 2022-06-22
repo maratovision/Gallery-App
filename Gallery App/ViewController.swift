077bd932f67f7aa9a716742f70f7a018c883d7b3
Neobis <neobis@iMac-Neobis.local>
2022-06-22 15:09:10 +0600

    made a new project, start of development

---
+++ Gallery App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery App
+//
+//  Created by Neobis on 22/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
