d3602c4c40ce1095c702a5e9e997f1d6ad856c6f
Beks <beks@MacBook-Pro-Beks.local>
2022-06-24 00:14:39 +0600

    renew all buttons and design of app

---
+++ Gallery App/ViewControllers/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Gallery App
+//
+//  Created by Neobis on 22/6/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Gallery App/ViewControllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Gallery App
+//
+//  Created by Neobis on 22/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
